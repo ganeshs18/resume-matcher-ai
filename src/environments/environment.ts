5250352ea860c4a0346c9be73352b83baf5d438d
Ganesh S <ganeshsundar46@gmail.com>
2025-07-07 03:21:32 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8888/resume-matcher/',
+  apiUrl: 'http://34.47.212.98:8888/resume-matcher/',
   googleClientId: '380128960255-9i1va5kj53hhi6b8lnggmbgo2psuco9k.apps.googleusercontent.com',
 };
